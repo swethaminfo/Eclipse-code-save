20266c5cf12a61d9d778545f48a52c5d7950288b
Swetha <Swetha@192.168.0.19>
2019-08-04 08:42:36 -0400

    Committing all files

---
+++ FirstJavaProject/src/com/firstJavaProject/MultiplicationRunner.java
@@ -0,0 +1,13 @@
+package com.firstJavaProject;
+
+public class MultiplicationRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		//table.print();
+		//table.print(10);
+		table.print(2,11,20);
+
+	}
+
+}
---
+++ FirstJavaProject/src/com/firstJavaProject/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package com.firstJavaProject;
+
+public class MultiplicationTable {
+	void print() {
+		for(int i = 1; i<=10; i++) {
+			System.out.printf("%d * %d = %d",5,i,5*i).println();
+		}
+	}
+	void print(int table) {
+		for(int i = 1; i<=10; i++) {
+			System.out.printf("%d * %d = %d",table,i,table * i).println();
+}
+	}
+	void print(int table,int from,int to) {
+		for(int i = from; i<=to; i++) {
+			System.out.printf("%d * %d = %d",table,i,table * i).println();
+}
+	}
+}
